f535faf275fa8a295a7b73482d812c71aab7ea90
everttigchelaar@gmail.com <everttigchelaar@gmail.com>
2023-12-21 14:48:01 +0100

    changes

--- code-generator/src/main/java/io/github/potjerodekool/openapi/internal/generate/Source.java
+++
@@ -1,4 +0,0 @@
-package io.github.potjerodekool.openapi.internal.generate;
-
-public interface Source {
-}
--- code-generator/src/main/java/io/github/potjerodekool/openapi/internal/generate/UnknownSource.java
+++
@@ -1,9 +0,0 @@
-package io.github.potjerodekool.openapi.internal.generate;
-
-public class UnknownSource implements Source {
-
-    public static final UnknownSource INSTANCE = new UnknownSource();
-
-    private UnknownSource() {
-    }
-}
